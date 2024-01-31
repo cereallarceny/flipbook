0d2e5c9244b92a47e5840b89cae696fa0f510e4b
akashahmad <engr.akashahmad@gmail.com>
2024-01-31 19:48:49 +0500

    test: benchmark reader with tinybench and playwright

--- packages/e2e/scripts/add-benchmarks-to-markdown.js
+++ packages/e2e/scripts/add-benchmarks-to-markdown.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const fs = require('fs/promises');
-const markdownInclude = require('markdown-include');
 
 // Generate markdown table from data
 function generateMarkdownTable(data) {
@@ -26,10 +25,14 @@ function generateMarkdownTable(data) {
   try {
     // structure data object
     const data = {
-      100: {},
-      '1k': {},
-      '10k': {},
-      '100k': {},
+      'writer-100': {},
+      'writer-1k': {},
+      'writer-10k': {},
+      'writer-100k': {},
+      'reader-100': {},
+      'reader-1k': {},
+      'reader-10k': {},
+      'reader-100k': {},
     };
 
     // output directory
@@ -39,28 +42,65 @@ function generateMarkdownTable(data) {
     const docsDir = path.resolve(__dirname, '..', 'docs');
 
     // read files
-    const hundred = await fs.readFile(`${outDir}/bench-hundred.json`, 'utf-8');
-    const thousand = await fs.readFile(`${outDir}/bench-1k.json`, 'utf-8');
-    const tenThousand = await fs.readFile(`${outDir}/bench-10k.json`, 'utf-8');
-    const hundredThousand = await fs.readFile(
-      `${outDir}/bench-100k.json`,
+    const writerHundred = await fs.readFile(
+      `${outDir}/writer-bench-hundred.json`,
+      'utf-8'
+    );
+    const writerThousand = await fs.readFile(
+      `${outDir}/writer-bench-1k.json`,
+      'utf-8'
+    );
+    const writerTenThousand = await fs.readFile(
+      `${outDir}/writer-bench-10k.json`,
+      'utf-8'
+    );
+    const writerHundredThousand = await fs.readFile(
+      `${outDir}/writer-bench-100k.json`,
+      'utf-8'
+    );
+    const readerHundred = await fs.readFile(
+      `${outDir}/reader-bench-hundred.json`,
+      'utf-8'
+    );
+    const readerThousand = await fs.readFile(
+      `${outDir}/reader-bench-1k.json`,
+      'utf-8'
+    );
+    const readerTenThousand = await fs.readFile(
+      `${outDir}/reader-bench-10k.json`,
+      'utf-8'
+    );
+    const readerHundredThousand = await fs.readFile(
+      `${outDir}/reader-bench-100k.json`,
       'utf-8'
     );
 
     // store data in data object
-    data[100] = JSON.parse(hundred);
-    data['1k'] = JSON.parse(thousand);
-    data['10k'] = JSON.parse(tenThousand);
-    data['100k'] = JSON.parse(hundredThousand);
+    data['writer-100'] = JSON.parse(writerHundred);
+    data['writer-1k'] = JSON.parse(writerThousand);
+    data['writer-10k'] = JSON.parse(writerTenThousand);
+    data['writer-100k'] = JSON.parse(writerHundredThousand);
+    data['reader-100'] = JSON.parse(readerHundred);
+    data['reader-1k'] = JSON.parse(readerThousand);
+    data['reader-10k'] = JSON.parse(readerTenThousand);
+    data['reader-100k'] = JSON.parse(readerHundredThousand);
 
     // generate markdown tables
-    const hundredTable = generateMarkdownTable([data[100]]);
-    const thousandTable = generateMarkdownTable([data['1k']]);
-    const tenThousandTable = generateMarkdownTable([data['10k']]);
-    const hundredThousandTable = generateMarkdownTable([data['100k']]);
+    const writerHundredTable = generateMarkdownTable([data['writer-100']]);
+    const writerThousandTable = generateMarkdownTable([data['writer-1k']]);
+    const writerTenThousandTable = generateMarkdownTable([data['writer-10k']]);
+    const writerHundredThousandTable = generateMarkdownTable([
+      data['writer-100k'],
+    ]);
+    const readerHundredTable = generateMarkdownTable([data['reader-100']]);
+    const readerThousandTable = generateMarkdownTable([data['reader-1k']]);
+    const readerTenThousandTable = generateMarkdownTable([data['reader-10k']]);
+    const readerHundredThousandTable = generateMarkdownTable([
+      data['reader-100k'],
+    ]);
 
     // create markdown
-    const markdown = `\n## Benchmarks\n\n### For 100 char string\n\n${hundredTable}\n\n### For 1,000 char string\n\n${thousandTable}\n\n### For 10,000 char string\n\n${tenThousandTable}\n\n### For 100,000 char string\n\n${hundredThousandTable}`;
+    const markdown = `\n## Benchmarks\n\n### Writer\n\n#### For 100 char string\n\n${writerHundredTable}\n\n#### For 1,000 char string\n\n${writerThousandTable}\n\n#### For 10,000 char string\n\n${writerTenThousandTable}\n\n#### For 100,000 char string\n\n${writerHundredThousandTable}\n\n### Reader\n\n#### For 100 char string\n\n${readerHundredTable}\n\n#### For 1,000 char string\n\n${readerThousandTable}\n\n#### For 10,000 char string\n\n${readerTenThousandTable}\n\n#### For 100,000 char string\n\n${readerHundredThousandTable}`;
 
     // save markdown
     await fs.writeFile(`${docsDir}/benchmarks.md`, markdown, 'utf-8');

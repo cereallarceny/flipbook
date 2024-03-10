2640813a379cd97e52fa28d404995ae6b07dfcd1
Patrick Cason <me@patrickcason.com>
2024-03-09 19:50:24 -0600

    chore: fixing linting

--- packages/shared/tsup.config.ts
+++ packages/shared/tsup.config.ts
@@ -1,4 +1,5 @@
 import { type Options, defineConfig } from 'tsup';
 import config from 'tsup-config/index.json';
 
+// eslint-disable-next-line import/no-default-export -- This is what tsup expects
 export default defineConfig(config as Options);

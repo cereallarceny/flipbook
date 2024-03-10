4a81102bc079be273999b6791f3a474f4fa72c28
Patrick Cason <me@patrickcason.com>
2024-03-09 20:05:21 -0600

    chore: removing shared package

--- packages/reader/tsup.config.ts
+++ packages/reader/tsup.config.ts
@@ -1,5 +1,13 @@
-import { type Options, defineConfig } from 'tsup';
-import config from 'tsup-config/index.json';
+import { defineConfig } from 'tsup';
 
 // eslint-disable-next-line import/no-default-export -- This is what tsup expects
-export default defineConfig(config as Options);
+export default defineConfig({
+  clean: true,
+  dts: true,
+  bundle: false,
+  splitting: true,
+  treeshake: true,
+  target: 'node18',
+  format: ['esm', 'cjs'],
+  entry: ['src/**/*.ts', '!src/**/*.test.ts'],
+});
